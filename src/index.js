c66a2ad48985a781bf64a9e57e608be1476b8383
Derrian Wilson <leander316@gmail.com>
2021-11-06 23:53:14 -0400

    adds a theme provider adds a temaplte card

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createTheme, ThemeProvider } from '@mui/material'
+
+const theme = createTheme({});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/pages/RoleModels.jsx
+++ src/pages/RoleModels.jsx
@@ -1,7 +1,44 @@
 import React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import AccessibilityIcon from '@mui/icons-material/Accessibility';
+
+import image from '../images/Photo by Emmy E from Pexels2381069.jpg'
+import styles from './styles/cardStyles'
 
 const RoleModels = () => {
-  return <div>ROLE MODELSSSSS</div>;
+  return(
+  <div>
+    <Card sx={{ maxWidth: 250 }}>
+      <CardMedia
+        component="img"
+        height="140"
+        image={image}
+        alt="green iguana"
+      />
+      <CardContent>
+        {/* <Typography gutterBottom variant="h5" component="div">
+          Elizibeth Stuart
+        </Typography> */}
+        <AccessibilityIcon />
+        <Typography variant="body2" color="text.secondary">
+          <Typography>Mentor Name</Typography>
+          <Typography>Nationality</Typography>
+          <Typography>Gender</Typography>
+
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Developer</Button>
+        <Button size="small">Writer</Button>
+      </CardActions>
+    </Card>
+  </div>
+  )
 };
 
 export default RoleModels;
